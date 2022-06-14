4c05f8a0fb6ba38df3842e74c7be48868eeb6ec5
Zachary Dowell <zdowell97@gmail.com>
2022-06-14 17:37:13 -0400

    second commit

--- assets/js/paint.js
+++ assets/js/paint.js
@@ -1,28 +1,30 @@
 function configureListeners() {
-    let images = document.getElementsByTagName('img');    
+    let images = document.getElementsByTagName('img')
 
-     for (let i = 0; i < images.length; i++) {        
-        document.getElementById(images[i].id).addEventListener('mouseover', addOpacity, false)        
-        document.getElementById(images[i].id).addEventListener('mouseout', removeOpacity, false)        
+
+     for (var i = 0; i < images.length; i++) {        
+        document.getElementById(images[i].id).addEventListener('mouseover',addOpacity,false)
+        document.getElementById(images[i].id).addEventListener('mouseout',removeOpacity,false)     
     } 
+
 }
 
 function addOpacity(event) {
-    if (!this.classList.contains('dim')){
+    if(!this.classList.contains('dim')) {
         this.classList.add('dim')
-    }    
+    }
     getProductInfo(event.target.id);     
 }
 
 function removeOpacity(event) {
-     if (this.classList.contains('dim')){
-        this.classList.remove('dim');
+     if(this.classList.contains('dim')) {
+        this.classList.remove('dim')
     }
-
+    
     let element = document.getElementById('color-price');
         element.textContent = '';
         
-    let color = document.getElementById('color-name');
+    let color = document.getElementById('color-Name');
         color.textContent = ''; 
 
     event.preventDefault();    
@@ -35,54 +37,53 @@ function getProductInfo(partNumber) {
     switch (partNumber) {
         case 'pn1':           
             price = '$14.99'
-            colorName = 'lime green'
-            updatePrice(colorName, price)     
+            colorName = 'Lime Green'
+            updatePrice(colorName, price)           
             break;           
         case 'pn2':
             price = '$11.14'
-            colorName = 'Medium Brown'
-            updatePrice(colorName, price)    
+            colorName = 'Medium Brown' 
+            updatePrice(colorName, price)     
             break;            
         case 'pn3':
             price = '$22.99'
-            colorName = 'Royal Blue'
-            updatePrice(colorName, price) 
-            break;   
-        case 'pn4':
-            price = '$4.99'
-            colorName = 'Solid Black'
-            updatePrice(colorName, price)
-            break;   
-        case 'pn5':
-            price = '$8.22'
-            colorName = 'Solid Cyan'
+            colorName = 'Royal Blue'   
             updatePrice(colorName, price)   
             break;   
+        case 'pn4':
+            price = '$13.42'
+            colorName = 'Solid Red'            
+            updatePrice(colorName, price)   
+            break;   
+        case 'pn5':
+            price = '$21.98'
+            colorName = 'Solid White' 
+            updatePrice(colorName, price)              
+            break;   
         case 'pn6':
-            price = '$11.99'
-            colorName = 'Solid Purple'
-            updatePrice(colorName, price)      
+            price = '$4.99'
+            colorName = 'Solid Black'   
+            updatePrice(colorName, price)            
             break;   
         case 'pn7':
-            price = '$13.42'
-            colorName = 'Solid Red'
-            updatePrice(colorName, price)
+            price = '$8.22'
+            colorName = 'Solid Cyan'       
+            updatePrice(colorName, price)        
             break;   
         case 'pn8':
-            price = '$21.98'
-            colorName = 'Solid white'
-            updatePrice(colorName, price)  
+            price = '$11.99'
+            colorName = 'solid Purple'      
+            updatePrice(colorName, price)         
             break;   
         case 'pn9':
             price = '$14.99'
-            colorName = 'Solid Yellow'
-            updatePrice(colorName, price)
+            colorName = 'Solid Yellow'                       
+            updatePrice(colorName, price)   
             break;   
           default:              
     }
 
-    function updatePrice(colorName, price)
-    {       
+    function updatePrice(colorName, price) {       
         let colorPrice = document.getElementById('color-price');
         colorPrice.textContent = price;
         
